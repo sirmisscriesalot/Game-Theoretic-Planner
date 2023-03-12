a7294b5f196727bb9446c23bceb0d8c26ddf63bf
Himadri Bhakta <himadribhakta@gmail.com>
2023-03-12 19:06:39 +0530

    working prototype spline

--- path2.py
+++ path2.py
@@ -14,15 +14,18 @@ second_powers = coeffs[2]
 first_powers = coeffs[1]
 no_powers = coeffs[0]
 
-x_1_3 = third_powers[1][0]
-x_1_2 = second_powers[1][0]
-x_1_1 = first_powers[1][0]
-x_1_0 = no_powers[1][0]
+k = 8
+x_1_3 = third_powers[k][0]
+x_1_2 = second_powers[k][0]
+x_1_1 = first_powers[k][0]
+x_1_0 = no_powers[k][0]
 
-y_1_3 = third_powers[1][1]
-y_1_2 = second_powers[1][1]
-y_1_1 = first_powers[1][1]
-y_1_0 = no_powers[1][1]
+print(x_1_0)
+
+y_1_3 = third_powers[k][1]
+y_1_2 = second_powers[k][1]
+y_1_1 = first_powers[k][1]
+y_1_0 = no_powers[k][1]
 
 test_points = cs(np.linspace(0,len(control_points)))
 test_points_2 = np.linspace(0 ,1)
@@ -37,19 +40,24 @@ def find_closest_point(params):
     nlp = {'x': t, 'f': distance, 'p': P}
 
     solver = ca.nlpsol('solver', 'ipopt', nlp)
-    sol = solver(p=params)
+    sol = solver(x0=0.5, lbx = 0, ubx = 1, p=params)
     t_opt = sol['x']
 
     return t_opt
 
-ans = find_closest_point(np.array([2, 2, x_1_0, x_1_1, x_1_2, x_1_3, y_1_0, y_1_1, y_1_2, y_1_3]))
+ans = find_closest_point(np.array([-1.5, 1.5, x_1_0, x_1_1, x_1_2, x_1_3, y_1_0, y_1_1, y_1_2, y_1_3]))
 print(ans)
-x = cs(1+ans)[0][0][0]
-y = cs(1+ans)[0][0][1]
+# x = cs(k+ans)[0][0][0]
+# y = cs(k+ans)[0][0][1]
 
-plt.plot([x], [y], 'ro')
-plt.plot([2],[2], 'ro')
-plt.plot(np.linspace(x,2), np.linspace(y,2), 'g--')
+ans = float(ans)
+print(ans)
+x = x_1_0*ans**3 + x_1_1*ans**2 + x_1_2*ans + x_1_3
+y = y_1_0*ans**3 + y_1_1*ans**2 + y_1_2*ans + y_1_3
+
+plt.plot(x, y, 'ro')
+plt.plot([-1.5],[1.5], 'ro')
+plt.plot(np.linspace(x,-1.5), np.linspace(y,1.5), 'g--')
 plt.gca().set_aspect('equal', adjustable='box')
 plt.plot([i[0] for i in test_points], [i[1] for i in test_points])
 #OK SO this is how you extract the coefficients of the spline piecewise polynomial
